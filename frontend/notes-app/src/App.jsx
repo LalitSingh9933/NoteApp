a8783b01504e0289852a0b8eac17cfb75a7dff30
Lalit Singh <nlalitsingh2014@gmail.com>
2025-07-28 21:44:22 +0545

    making login default

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,25 +1,23 @@
-import { useState } from 'react'
-import Home from './pages/Home/Home'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login/Login'
-import SignUp from './pages/SignUp/SignUp'
+import { useState } from 'react';
+import Home from './pages/Home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
 
-const routes = (
-  <Router>
-    <Routes>
-      <Route path='/dashboard' exact element={<Home />} />
-      <Route path='/login' exact element={<Login />} />
-      <Route path='/signup' exact element={<SignUp />} />
-    </Routes>
-  </Router>
-)
 function App() {
-
   return (
-    <div>
-      {routes}
-    </div>
-  )
+    <Router>
+      <Routes>
+       
+        <Route path='/' element={<Login />} />
+        <Route path='/dashboard' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
+
+
